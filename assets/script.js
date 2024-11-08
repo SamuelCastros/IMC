936b4cd89ccfffa483cd2f63c759974648b8e15a
SamuelCastros <samuelcastrosilva021@gmail.com>
2024-11-08 17:40:15 -0300

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -1,71 +1,78 @@
-function meuEscopo(){
     const form = document.querySelector('.form')
-    const status = document.querySelector('.status')
-  
-  
 
-    function recebeEventoForm(evento){
-        evento.preventDefault() ;
+    form.addEventListener('submit',function(e){
+        e.preventDefault();
+        const inputPeso = e.target.querySelector('.peso');
+        const inputAltura = e.target.querySelector('.altura');
 
-    let peso = document.querySelector('.peso')
-    let altura = document.querySelector('.altura')
+        const peso = Number(inputPeso.value);
+        const altura = Number(inputAltura.value.replace(",","."));
+        const validaAltura = altura <0.5 || altura >2.5
+        const validaPeso = peso <2 || peso >500
+
+        if(!peso|| validaPeso){
+            setResultado('Peso inválido', false)
+            return;
+        }
+    
+        if(!altura || validaAltura){
+            setResultado('Altura inválida', false)
+            return;
+        }
 
 
-    let valorA  = parseFloat(altura.value.replace(",", "."));
-    let valorP = peso.value
+
+        const imc = getImc(peso,altura); 
+        const nivelImc = getNivelImc(imc);
+
+        const msg = `Seu imc é ${imc} (${nivelImc})`;
+        setResultado(msg,true);
+
+    });
+
+    function getNivelImc(imc){
+        const nivel = ['Abaixo do peso','Peso normal','Sobrepeso', 
+            'Obesidade grau 1','Obesidade grau 2', 'Obesidade grau 3']
+
+            if(imc>=39.9)return nivel[5]
+            if(imc>=34.9)return nivel[4]
+            if(imc>=29.9)return nivel[3]
+            if(imc>=24.9)return nivel[2]
+            if(imc>=18.5)return nivel[1]
+            if(imc<18.5)return nivel[0]
+    }
+
+    function getImc(peso,altura){
+        const imc = peso/altura**2;
+        return imc.toFixed(2);
+    }
+
+    function criaP(){
+        const p = document.createElement('p'); 
+        return p ;
+    }
+
+    function setResultado(msg, isValid){
+        const resultado = document.querySelector('.status')
+        resultado.innerHTML=' '; 
+
+        const p = criaP()
+
+        if(isValid){
+            p.classList.add('paragrafo-resultado'); 
+        }else{
+            p.classList.add('bad');
+        }
+
+        p.innerHTML = msg; 
+        resultado.appendChild(p);
+    }
     
 
-  
-    const calculo = valorP/ (valorA * valorA)
-
-    const validacao = isNaN(valorP) || isNaN(valorA)
-    const validaCampo = valorP.length=== 0 ||  valorA.length=== 0
-    const validaAltura = valorA<0.5 || valorA >2.5
-    const validaPeso = valorP <=2 || valorP >500 
-    
-    if(calculo<18.5)(
-        status.innerHTML =  (`<p> o peso é: <strong> ${valorP} </strong> <br>a altura é: ${valorA} <br> O IMC é: ${calculo.toFixed(2)} <br>  <strong> ABAIXO</strong></p>`)
-    ) 
-    else if(calculo>=18.5 && calculo <= 24.9){
-        status.innerHTML =  (`<p> o peso é: <strong> ${valorP} </strong> <br>a altura é: ${valorA.toFixed(2)} <br> O IMC é: ${calculo.toFixed(2)} <br> <strong> NORMAL</strong></p>`)
-    } 
-    else if(calculo>=25 && calculo<=29.9){
-        status.innerHTML =  (`<p> o peso é: <strong> ${valorP} </strong> <br>a altura é: ${valorA.toFixed(2)} <br> O IMC é: ${calculo.toFixed(2)}  <br>  <strong> SOBREPESO</strong></p>`)
-    }
-    else if (calculo>=30 && calculo<=34.9){
-        status.innerHTML =  (`<p> o peso é: <strong> ${valorP} </strong> <br>a altura é: ${valorA.toFixed(2)} <br> O IMC é: ${calculo.toFixed(2)} <br> <strong> Obesidade grau 1</strong></p>`)
-    }
-    else if(calculo >=35 && calculo <=39.9){
-        status.innerHTML =  (`<p> o peso é: <strong> ${valorP} </strong> <br>a altura é: ${valorA.toFixed(2)} <br> O IMC é: ${calculo.toFixed(2)} <br> <strong> Obesidade grau 2</strong></p>`)
-    }
-    else if (calculo>40){
-        status.innerHTML =  (`<p> o peso é: <strong> ${valorP} </strong> <br>a altura é: ${valorA.toFixed(2)} <br> O IMC é: ${calculo.toFixed(2)} <br> <strong> Obesidade grau 3</strong></p>`)
-    }
-        
- 
-    if(validacao || validaAltura|| validaPeso){
-        status.innerHTML =  (`<p> <strong> Verifique peso e altura </strong> </p>`)
-    }
-
-    if(validaCampo){
-        status.innerHTML =  (`<p> <strong> Preencha os campos </strong> </p>`)
-    }
-
-
-   
-
-   
-
-
-    }
-
-    form.addEventListener("submit", recebeEventoForm); 
-
-
-}
-
-
-
     
 
-meuEscopo()
+
+
+
+
+
